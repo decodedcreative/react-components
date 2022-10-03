569d474f7478926456d3998d062d14cb36fe45a2
James Howell <me@jameshowell.dev>
2022-10-03 22:08:10 +0100

    test(radio): added further unit test coverage

--- libs/react-components/src/lib/components/forms/radio/radio.spec.tsx
+++ libs/react-components/src/lib/components/forms/radio/radio.spec.tsx
@@ -24,24 +24,49 @@ describe('Radio', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should have an aria-invalid attribute on its input if in an error state', () => {
-    const component = createComponentRender({
-      label: 'Radio label',
-      'aria-invalid': true,
-      'data-testid': 'radio-input',
-    } as RadioProps);
-    const input = component.getByTestId('radio-input');
-    expect(input).toHaveAttribute('aria-invalid');
+  it('should match the default snapshot', () => {
+    const defaultRadio = createComponentJSX({} as RadioProps);
+    const defaultRadioJSON = renderer.create(defaultRadio).toJSON();
+    expect(defaultRadioJSON).toMatchSnapshot();
   });
 
-  it('should disable its input if a disabled prop is supplied', () => {
-    const component = createComponentRender({
-      label: 'Radio label',
-      disabled: true,
-      'data-testid': 'radio-input',
-    } as RadioProps);
-    const input = component.getByTestId('radio-input');
-    expect(input).toBeDisabled();
+  describe('Radio - States', () => {
+    describe('Radio - States - Error', () => {
+      it('should have an aria-invalid attribute on its input if in an error state', () => {
+        const component = createComponentRender({
+          label: 'Radio label',
+          'aria-invalid': true,
+          'data-testid': 'radio-input',
+        } as RadioProps);
+        const input = component.getByTestId('radio-input');
+        expect(input).toHaveAttribute('aria-invalid');
+      });
+      it('should match the Radio With Error snapshot', () => {
+        const errorRadio = createComponentJSX({
+          'aria-invalid': true,
+        } as RadioProps);
+        const errorRadioJSON = renderer.create(errorRadio).toJSON();
+        expect(errorRadioJSON).toMatchSnapshot();
+      });
+    });
+    describe('Radio - States - Disabled', () => {
+      it('should disable its input if a disabled prop is supplied', () => {
+        const component = createComponentRender({
+          label: 'Radio label',
+          disabled: true,
+          'data-testid': 'radio-input',
+        } as RadioProps);
+        const input = component.getByTestId('radio-input');
+        expect(input).toBeDisabled();
+      });
+      it('should match the Disabled Radio snapshot', () => {
+        const disabledRadio = createComponentJSX({
+          disabled: true,
+        } as RadioProps);
+        const disabledRadioJSON = renderer.create(disabledRadio).toJSON();
+        expect(disabledRadioJSON).toMatchSnapshot();
+      });
+    });
   });
 
   it('should display its label', () => {
